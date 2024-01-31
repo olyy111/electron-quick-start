405112574b3f2d4aa13aea794804e8cfc44b2803
Liang Zhang <olyy111@gmail.com>
2024-01-31 12:02:52 +0800

    try deeplink

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, session, dialog, protocol, net, ipcMain} = require('electron')
 const path = require('path')
+const log = require('electron-log/main');
+const url = require('url')
+const { NsisUpdater } = require("electron-updater")
+
+log.initialize();
+log.transports.file.archiveLogFn = function archiveLog(file) {
+  file = file.toString();
+  const info = path.parse(file);
+  
+  try {
+    fs.renameSync(file, path.join(info.dir, info.name + '.old' + info.ext));
+  } catch (e) {
+    console.warn('Could not rotate log', e);
+  }
+}
+log.transports.file.maxSize = 10000
+
+
+const options = {
+  requestHeaders: {
+      // Any request headers to include here
+  },
+  provider: 'generic',
+  url: 'http://localhost:8080'
+}
+
+const autoUpdater = new NsisUpdater(options)
+autoUpdater.checkForUpdatesAndNotify()
+autoUpdater.on('update-available', () => {
+  dialog.showErrorBox('有更新可以用了')
+})
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     }
   })
 
@@ -17,7 +50,20 @@ function createWindow () {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+  ipcMain.on('getTutorialUrl', () => {
+    console.log('2234')
+  })
 }
+
+app.setAsDefaultProtocolClient('foobar');
+protocol.registerSchemesAsPrivileged([
+  {
+    scheme: 'ffdra',
+    privileges: {
+        stream: true,
+    }
+  }
+])
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -30,6 +76,41 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
+  session.defaultSession.webRequest.onHeadersReceived({urls: [`*://*/secretapi/login`]}, (details, callback) => {
+    global.console.log('details=>', details)
+    callback({ responseHeaders: {...details.responseHeaders, a: 'haha'}, test: 'textContent' });
+    })
+  
+})
+
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    log.info(`[listener:second-instance] ${commandLine} | ${workingDirectory}`)
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+    // the commandLine is array of strings in which last element is deep link url
+    dialog.showErrorBox('Welcome Back', `You arrived from: ${commandLine.pop()}`)
+    console.log('Welcome Back', `You arrived from: ${url}`)
+  })
+
+
+  // Create mainWindow, load the rest of the app, etc...
+  // app.whenReady().then(() => {
+  //   createWindow()
+  // })
+}
+
+app.on('open-url', (event, url) => {
+  dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
+  log.info(`[listener:open-url] ${url}`)
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
--- renderer.js
+++ renderer.js
@@ -1,7 +1,95 @@
-/**
- * This file is loaded via the <script> tag in the index.html file and will
- * be executed in the renderer process for that window. No Node.js APIs are
- * available in this process because `nodeIntegration` is turned off and
- * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
- * to expose Node.js functionality from the main process.
- */
+const {
+    createAgoraRtcEngine,
+    VideoMirrorModeType,
+    VideoSourceType,
+    RenderModeType,
+    ChannelProfileType,
+    ClientRoleType,
+    AudienceLatencyLevelType,
+    } = require("agora-electron-sdk");
+
+let rtcEngine;
+let localVideoContainer;
+let remoteVideoContainer;
+let isJoined = false;
+// 填入你的 App ID
+const APPID = "8954b25852ef4994a275b3c8e87684b2";
+// 填入你的临时 Token
+let token = "007eJxTYCgszz34MLRi9abyvf188a0NUye//3CVrWQKsxfPk893WksVGCwsTU2SjEwtTI1S00wsLU0SjcxNk4yTLVItzM0sgDLt/PNSGwIZGVofsjAzMkAgiM/CUJJaXMLAAACoryAZ";
+// 填入生成 Token 时使用的频道名
+const channel = "test";
+// 用户 ID，并确保其在频道内的唯一性
+let uid = 123;
+
+const EventHandles = {
+    // 监听本地用户加入频道事件
+    onJoinChannelSuccess: ({ channelId, localUid }, elapsed) => {
+        console.log('成功加入频道：' + channelId);
+        isJoined = true;
+        // 本地用户加入频道后，设置本地视频窗口
+        rtcEngine.setupLocalVideo({
+            sourceType: VideoSourceType.VideoSourceCameraPrimary,
+            view: localVideoContainer,
+            mirrorMode: VideoMirrorModeType.VideoMirrorModeDisabled,
+            renderMode: RenderModeType.RenderModeFit,
+        });
+    },
+
+    // 监听用户离开频道事件
+    onLeaveChannel: ({ channelId, localUid }, stats) => {
+        console.log('成功退出频道：' + channelId);
+        isJoined = false;
+    },
+
+    // 监听远端用户加入频道事件
+    onUserJoined: ({ channelId, localUid }, remoteUid, elapsed) => {
+        console.log('远端用户 ' + remoteUid + ' 已加入');
+        // 远端用户加入频道后，设置远端视频窗口
+        rtcEngine.setupRemoteVideoEx(
+            {
+                sourceType: VideoSourceType.VideoSourceRemote,
+                uid: remoteUid,
+                view: remoteVideoContainer,
+                mirrorMode: VideoMirrorModeType.VideoMirrorModeDisabled,
+                renderMode: RenderModeType.RenderModeFit,
+            },
+            { channelId },
+        );
+    },
+};
+
+
+window.onload = () => {
+    const os = require("os");
+    const path = require("path");
+    localVideoContainer = document.getElementById("join-channel-local-video");
+    remoteVideoContainer = document.getElementById("join-channel-remote-video");
+    const sdkLogPath = path.resolve(os.homedir(), "./test.log");
+
+    // 创建 RtcEngine 实例
+    rtcEngine = createAgoraRtcEngine();
+
+    // 初始化 RtcEngine 实例
+    rtcEngine.initialize({
+        appId: APPID,
+        logConfig: { filePath: sdkLogPath }
+    });
+
+    // 注册事件回调
+    rtcEngine.registerEventHandler(EventHandles);
+
+    // 设置频道场景为直播场景
+    rtcEngine.setChannelProfile(ChannelProfileType.ChannelProfileLiveBroadcasting);
+
+    // 设置用户角色，主播设为 ClientRoleBroadcaster，观众设为 ClientRoleAudience
+    rtcEngine.setClientRole(ClientRoleType.ClientRoleBroadcaster);
+
+    // 视频默认禁用，你需要调用 enableVideo 启用视频流
+    rtcEngine.enableVideo();
+
+    // 开启摄像头预览
+    rtcEngine.startPreview();
+
+    // 使用临时 Token 加入频道
+    rtcEngine.joinChannel(token, channel, uid);
+};
